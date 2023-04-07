e6df4e3cc4fe8f9c9dad715c86552e5bd70537c2
Ahmed Abdelaziz <102368041+AhmedAbdelaziz0@users.noreply.github.com>
2023-04-07 20:34:22 +0200

    Apply some cahanges to be more like tiny

--- include/visitor.hpp
+++ include/visitor.hpp
@@ -9,26 +9,29 @@ public:
   // A virtual destructor
   virtual ~Visitor() {}
 
-  // A pure virtual function to visit a binary expression node
-  virtual void visit(BinaryExpr *node) = 0;
+  virtual void visit(Block *node) = 0;
 
-  // A pure virtual function to visit an integer literal node
+  virtual void visit(Program *node) = 0;
+
+  virtual void visit(Comparison *node) = 0;
+
+  virtual void visit(Expression *node) = 0;
+
+  virtual void visit(Term *node) = 0;
+  
+  virtual void visit(Unary *node) = 0;
+
   virtual void visit(IntLiteral *node) = 0;
 
-  // A pure virtual function to visit the identifer (int) node
+  virtual void visit(DoubleLiteral *node) = 0;
+
+  virtual void visit(StringLiteral *node) = 0;
+
   virtual void visit(Identifer *node) = 0;
 
-  // A pure virtual function to visit the variable (int var) node
-  virtual void visit(Variable *node) = 0;
-
-  // A pure virtual function to visit the declaration node (int a = 5;)
   virtual void visit(Declare *node) = 0;
 
+  virtual void visit(Print *node) = 0;
+  
   virtual void visit(Assign *node) = 0;
-
-  // A pure virtual function to visit the Statement (start statement and finish
-  // with ;)
-  virtual void visit(Statement *node) = 0;
-
-  virtual void visit(Block *node) = 0;
 };
